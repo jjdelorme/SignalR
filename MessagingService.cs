725635a1c1ac4029f39f4ff81c944dc0a86c2b6e
Jason De Lorme <delormej@gmail.com>
2021-09-27 21:22:16 -0400

    Adding service to Pull messages from PubSub.

---
+++ MessagingService.cs
@@ -0,0 +1,105 @@
+using Text = System.Text;
+using System.Threading.Tasks;
+using Google.Cloud.PubSub.V1;
+using Grpc.Core;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.SignalR;
+
+namespace signalR
+{
+    public class MessagingService : IHostedService
+    {
+        private ILogger _log;
+        private string _projectId;
+        private string _topicId;
+        private string _subscriptionId;
+        private IHubContext<ChatHub> _hub;
+        private SubscriptionName? _subscriptionName;
+
+        public MessagingService(ILogger logger, IConfiguration config, IHubContext<ChatHub> hub)
+        {
+            _log = logger;
+            _subscriptionId = new Guid().ToString();
+            _topicId = config["TopicId"];
+            _projectId = config["ProjectId"];
+            _hub = hub;
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            CreateSubscription();
+
+            SubscriberClient subscriber = 
+                await SubscriberClient.CreateAsync(_subscriptionName);
+
+            await subscriber.StartAsync(ProcessMessageAsync);   
+        }
+
+        public async Task StopAsync(CancellationToken cancellationToken)
+        {
+            SubscriberClient subscriber = 
+                await SubscriberClient.CreateAsync(_subscriptionName);
+            
+            await subscriber.StopAsync(CancellationToken.None);
+            
+            DeleteSubscription();
+        }
+
+        private void DeleteSubscription()
+        {
+            SubscriberServiceApiClient subscriber = SubscriberServiceApiClient.Create();
+            subscriber.DeleteSubscription(_subscriptionName);            
+        }
+
+        private void CreateSubscription()
+        {
+            SubscriberServiceApiClient subscriber = SubscriberServiceApiClient.Create();
+            TopicName topicName = TopicName.FromProjectTopic(_projectId, _topicId);
+
+            _subscriptionName = SubscriptionName.FromProjectSubscription(
+                _projectId, _subscriptionId);
+            Subscription? subscription = null;
+
+            try
+            {
+                subscription = subscriber.CreateSubscription(
+                    _subscriptionName, topicName, pushConfig: null, ackDeadlineSeconds: 60);
+            }
+            catch (RpcException e) when (e.Status.StatusCode == StatusCode.AlreadyExists)
+            {
+                // Already exists.  That's fine.
+            }
+        }
+
+        private async Task<SubscriberClient.Reply> ProcessMessageAsync(
+            PubsubMessage message, 
+            CancellationToken cancel)
+        {
+            SubscriberClient.Reply reply = SubscriberClient.Reply.Nack;
+
+            if (cancel.IsCancellationRequested)
+            {
+                return reply;
+            }
+
+            try
+            {               
+                // Process the message.
+                string json = Text.Encoding.UTF8.GetString(message.Data.ToArray());
+                _log.LogInformation($"Received message id:{message.MessageId}, Body:{json}");
+
+                await _hub.Clients.All.SendAsync("ReceiveMessage", json);
+
+                reply = SubscriberClient.Reply.Ack;
+            }
+            catch (Exception e)
+            {
+                _log.LogError($"ERROR: Attempt for message {message.MessageId}.", e);
+            }
+            
+            _log.LogInformation($"Message handler completed with {reply} for {message.MessageId}.");
+            
+            return reply;
+        }
+   }
+}
--- Program.cs
+++ Program.cs
@@ -1,8 +1,8 @@
-using Microsoft.AspNetCore.SignalR;
 using signalR;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSignalR();
+builder.Services.AddHostedService<MessagingService>();
 
 var app = builder.Build();
 
@@ -14,16 +14,4 @@ app.UseEndpoints(endpoints =>
     endpoints.MapHub<ChatHub>("/chathub")
 );
 
-void SendMessages(WebApplication app)
-{
-    var hub = app.Services.GetRequiredService<IHubContext<ChatHub>>();
-    var clients = hub.Clients.All;
-    var timer = new Timer(new TimerCallback( 
-        async (_) => await clients.SendAsync("ReceiveMessage", "Welcome")),
-        null, 1000, 2000
-    );
-}
-
-SendMessages(app);
 app.Run();
-
